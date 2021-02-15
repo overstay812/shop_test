49218eb096bee260ac1f4bd1b0472782e4cfa5a3
overstay812 <overstay812@gmail.com>
2021-02-15 16:48:55 +0300

    created project

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+// $(function (){
+    $('.search-block-three').slick({
+        dots: false,
+        infinite: true,
+        speed: 10000,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        // centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4,
+                  infinite: true,
+                  dots: false
+                }
+              },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 490,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+// })
+
